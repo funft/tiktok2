ef03e6c149572e1657aae052bb2cbaeaf6d2face
funft <maiducvuong2002@gmail.com>
2022-07-03 09:20:33 +0700

    lam xong vid router/layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { Link, Routes, Route } from 'react-router-dom'
+import { publicRoutes } from '~/routes'
+import { DefaultLayout } from '~/components/Layout'
 import './App.css';
+import { Fragment } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <h1>react app</h1>
+      <Routes>
+        {publicRoutes.map((route, i) => {
+          const Layout = route.layout === null ? Fragment : (route.layout || DefaultLayout)
+          const Page = route.component
+
+          return <Route
+            key={i}
+            path={route.path}
+            element={
+              <Layout>
+                <Page />
+              </Layout>} />
+        })}
+      </Routes>
     </div>
   );
 }
---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+//Layout
+import { HeaderOnly } from '~/components/Layout';
+
+import Homepage from '~/pages/Home'
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+const publicRoutes = [
+    { path: '/', component: Homepage },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/search', component: Search, layout: null },
+    { path: '/upload', component: Upload, layout: HeaderOnly }
+]
+
+const privateRoutes = [
+
+]
+
+export { publicRoutes, privateRoutes }
